124521bf08ed2807afbc3689c9775083b0c8ca53
Douglas Mitchell <roxonus@gmail.com>
2020-07-20 14:00:04 -0400

    added date, time, and variables

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+var today = moment().format("dddd, MMMM Do, YYYY hh:mm a");
+$("#currentDay").text(today);
+var now = (moment()).toString();
+var today9Am = (moment('09:00', 'HH:mm')).toString();
+var today10Am = (moment('10:00', 'HH:mm')).toString();
+var today11Am = (moment('11:00', 'HH:mm')).toString();
+var today12Pm = (moment('12:00', 'HH:mm')).toString();
+var today1Pm = (moment('13:00', 'HH:mm')).toString();
+var today2Pm = (moment('14:00', 'HH:mm')).toString();
+var today3Pm = (moment('15:00', 'HH:mm')).toString();
+var today4Pm = (moment('16:00', 'HH:mm')).toString();
+var today5Pm = (moment('17:00', 'HH:mm')).toString();
+var today6Pm = (moment('18:00', 'HH:mm')).toString();
+
